13a943029c48ecdf1d3ad6f98b73a57a9547bb93
milselarch <milselarch@gmail.com>
2020-12-02 15:19:54 +0800

    add documentation

--- board.py
+++ board.py
@@ -6,6 +6,16 @@ import copy
 
 
 class Board(object):
+    """
+    This board object is intended as a wrapper for the
+    tictactoe board used in game.py, but with the additional
+    benefit that we can attach an event trigger that fires when
+    the board is being updated.
+
+    The idea is here is that we can attach a function to update
+    the GUI board every time the board state is being updated  
+    """
+    
     def __init__(self, init_board):
         self.init_board = copy.deepcopy(init_board)
         self.trigger = lambda *x: None
@@ -15,10 +25,12 @@ class Board(object):
         self.trigger = trigger
 
     def __setitem__(self, key, item):
+        # implement key-value assignment for wrapper object
         self.board[key] = item
         self.trigger(self.board)
     
     def __getitem__(self, key):
+        # implement read using key functionality for wrapper object
         return self.board[key]
 
 
@@ -41,6 +53,11 @@ class BoardGui(tk.Frame):
         )
 
     def reset_board(self):
+        """
+        Reset the GUI board such that is displays only
+        the numerical positions of each tile, and sets
+        the color of each tile to a light grey
+        """
         for k in range(3):
             for i in range(3):
                 string_var = self.state[(k, i)]
@@ -49,8 +66,18 @@ class BoardGui(tk.Frame):
                 label.config(fg='grey52')
 
     def set_board(self, board):
+        """
+        given a dictionary representing the state of the tictactoe
+        board, update the corresponding tiles in the GUI. 
+
+        The board dictionary should contains string keys 
+        '1', '2', ..., '9', and values corresponding to either
+        'O', 'X', or ' '
+        """
+
         for key in board:
             str_key = str(key)
+            assert str_key in ('O', 'X', ' ')
 
             if board[str_key] == ' ':
                 continue
@@ -65,6 +92,11 @@ class BoardGui(tk.Frame):
             label.config(fg='black')
 
     def make(self):
+        """
+        buffers here are expandable widgets
+        used to make the main tictactoe board center
+        regardless of how the window is stretched or shrunk
+        """
         self.left_buffer = tk.Frame(self, width=10, height=10)
         self.left_buffer.grid(row=100, column=000, sticky="nsew")
         self.right_buffer = tk.Frame(self, width=10, height=10)
@@ -75,12 +107,14 @@ class BoardGui(tk.Frame):
         self.bottom_buffer.grid(row=200, column=100, sticky="nsew")
 
         self.holder = tk.Frame(self)
+        # make rows 0, 200, columns 0, 200 expandable
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(200, weight=1)
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(200, weight=1)
         self.holder.grid(row=100, column=100)
         
+        # initialize labels representing tiles in board
         for k in range(3):
             for i in range(3):
                 string_var = tk.StringVar()
--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from threading import Thread
 # https://docs.python.org/3/library/threading.html
 # https://benedictwilkinsai.github.io/post/tkinter-mp/
 
-print('test')
-
+# create GUI window
 gui_board = board.init_board(start=False)
+# wrap game board with board.Board
 game.board = board.Board(game.board)
+# attach write event hook to wrapper board object
 game.board.attach_trigger(gui_board.set_board)
+
+# start terminal-based game
 thread = Thread(target=game.game)
 thread.start()
 
+# run gui main event loop
 gui_board.mainloop()
     
